754bb16543ba02d1f7d2fdc8307edcc29caa33cd
hassaan haider <hassaanhaider088@gmail.com>
2025-10-02 19:48:50 +0500

    Update Seo And FavIcon

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -13,7 +13,7 @@ const SingleProject = ({ImgData,id,handleImgViewerShown}) => {
    <>
     <div id={id} className={`xsm:flex block m-0 p-3 gap-5 justify-evenly ${ImgData.FlexDirecton} shadow-2xl rounded-xl `}>
         <div onClick={()=>handleImgViewerShown(ImgData?.ProjectImgURl)} className='rounded-xl w-full xsm:w-1/2 h-[45vh] overflow-hidden OuterProjectImgContainer'>
-     <img className="w-full duration-200 cursor-[url('../../AboutMeImg.png')] overflow-hidden"  src={ImgData.ProjectImgURl}  alt="" />
+     <img loading='lazy' className="w-full duration-200 cursor-[url('../../AboutMeImg.png')] overflow-hidden"  src={ImgData.ProjectImgURl}  alt="" />
         </div>
     <div className='relative flex flex-col justify-center items-center w-full xsm:w-1/2 text-center'>
         <h1 className='font-bold text-sm xsm:text-2xl'>{ImgData.ProjectImgName}</h1>
